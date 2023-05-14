e92af064a187afc693ec7db5d0983d8b8c3beb39
Siyana Danchilova <siyanadanchilova@gmail.com>
2023-05-14 11:22:21 +0300

    Update

---
+++ stacksAndQueuesExercises/InfixToPostfix08.java
@@ -0,0 +1,13 @@
+package stacksAndQueuesExercises;
+
+import java.util.Scanner;
+
+public class InfixToPostfix08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+         
+
+
+    }
+}
